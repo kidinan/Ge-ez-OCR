42e6d60bbcd84cecc6745e1850877a2d1cecaac3
Kidist Dejene <134932977+kidinan@users.noreply.github.com>
2024-11-08 12:42:22 +0300

    Update yolo_service.py

--- app/services/yolo_service.py
+++ app/services/yolo_service.py
@@ -1,31 +1,24 @@
 import os
 import torch
+import boto3
 from PIL import Image
 from io import BytesIO
 from dotenv import load_dotenv
 from fastapi import UploadFile
-import urllib.error
-import time
 
 # Load environment variables
 load_dotenv()
-yolo_model_path = os.getenv('YOLO_MODEL_PATH')
+s3_bucket_name = os.getenv('S3_BUCKET_NAME')
+s3_model_path = os.getenv('S3_MODEL_PATH')
+local_model_path = '/tmp/yolo_model.pt'  # Temporary path to save the model
 
-def load_yolo_model_with_retries(repo, model, path, retries=3, delay=5):
-    for attempt in range(retries):
-        try:
-            model = torch.hub.load(repo, model, path=path, trust_repo=True)
-            return model
-        except urllib.error.HTTPError as e:
-            if e.code == 403 and 'rate limit exceeded' in str(e):
-                print(f'Rate limit exceeded, retrying in {delay} seconds...')
-                time.sleep(delay)
-            else:
-                raise e
-    raise Exception('Failed to load model after multiple retries')
+def download_model_from_s3():
+    s3 = boto3.client('s3')
+    s3.download_file(s3_bucket_name, s3_model_path, local_model_path)
 
-# Load YOLO model with retry mechanism
-yolo_model = load_yolo_model_with_retries('ultralytics/yolov5', 'custom', path=yolo_model_path)
+# Download YOLO model from S3
+download_model_from_s3()
+yolo_model = torch.load(local_model_path)
 
 async def predict(file: UploadFile):
     contents = await file.read()
