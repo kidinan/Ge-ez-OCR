52bb57a4174f48517cfa2a381fb43cb4912cd3cc
kidinan <kidist338@gmail.com>
2024-11-09 01:07:56 +0300

    update

--- app/models/yolo_model.py
+++ app/models/yolo_model.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 def load_yolo_model():
     # Load environment variables
     load_dotenv()
-    yolo_model_path = os.getenv('YOLO_MODEL_PATH', 'models/yolo_model.pt')
+    yolo_model_path = os.getenv('YOLO_MODEL_PATH')
 
     # Load YOLO model from local file
     yolo_model = torch.load(yolo_model_path, map_location=torch.device('cpu'))
