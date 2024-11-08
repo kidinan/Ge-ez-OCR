8cc8b9aa2408eca827184993aa14fc6d7d3ce7b3
kidinan <kidist338@gmail.com>
2024-11-09 01:28:13 +0300

    update

--- app/models/yolo_model.py
+++ app/models/yolo_model.py
@@ -7,6 +7,13 @@ def load_yolo_model():
     load_dotenv()
     yolo_model_path = os.getenv('YOLO_MODEL_PATH')
 
+    if not yolo_model_path:
+        raise ValueError("Environment variable YOLO_MODEL_PATH is not set.")
+    if not os.path.isfile(yolo_model_path):
+        raise ValueError(f"Model file not found at {yolo_model_path}")
+    
+    print(f"Loading YOLO model from {yolo_model_path}")
+
     # Load YOLO model from local file
     yolo_model = torch.load(yolo_model_path, map_location=torch.device('cpu'))
     return yolo_model
