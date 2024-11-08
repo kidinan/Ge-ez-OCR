55904575ad5c494f6ac6cd1d276145a253da963f
Kidist Dejene <134932977+kidinan@users.noreply.github.com>
2024-11-08 13:13:44 +0300

    Update yolo_service.py

--- app/services/yolo_service.py
+++ app/services/yolo_service.py
@@ -1,24 +1,15 @@
 import os
 import torch
-import boto3
 from PIL import Image
 from io import BytesIO
 from dotenv import load_dotenv
-from fastapi import UploadFile
 
 # Load environment variables
 load_dotenv()
-s3_bucket_name = os.getenv('S3_BUCKET_NAME')
-s3_model_path = os.getenv('S3_MODEL_PATH')
-local_model_path = '/tmp/yolo_model.pt'  # Temporary path to save the model
+yolo_model_path = os.getenv('YOLO_MODEL_PATH')
 
-def download_model_from_s3():
-    s3 = boto3.client('s3')
-    s3.download_file(s3_bucket_name, s3_model_path, local_model_path)
-
-# Download YOLO model from S3
-download_model_from_s3()
-yolo_model = torch.load(local_model_path)
+# Load YOLO model
+yolo_model = torch.hub.load('ultralytics/yolov5', 'custom', path=yolo_model_path)
 
 async def predict(file: UploadFile):
     contents = await file.read()
